19e2717bad4142dedcad3e1a2a3031cb4543fe78
AhmedAlTahtawy01 <ahmedaltahtawy01@gmail.com>
2025-02-14 20:08:13 +0200

    Initial commit - Web API project setup

---
+++ MoveSmart/MoveSmartAPIs/Controllers/ClassController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace MoveSmartAPIs.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ClassController : ControllerBase
+    {
+       
+    }
+}
