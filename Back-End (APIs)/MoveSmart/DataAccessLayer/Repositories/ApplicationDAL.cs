f5cddcd15eff0433603eea58a067839929bb281c
AhmedAlTahtawy01 <ahmedaltahtawy01@gmail.com>
2025-02-16 23:09:42 +0200

    v1 of Level1 and Level2 DALs

---
+++ Back-End (APIs)/MoveSmart/DataAccessLayer/Repositories/ApplicationDAL.cs
@@ -0,0 +1,301 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Repositories
+{
+    public class Application
+    {
+        public int ApplicationId { get; set; }
+        public DateTime CreationTime { get; set; }
+        public int Status { get; set; }
+        public int ApplicationType { get; set; }
+        public string ApplicationDescription { get; set; }
+        public int CreatedByUser { get; set; }
+
+        public Application(int applicationId, DateTime creationTime, int status, int applicationType, string applicationDescription, int createdByUser)
+        {
+            this.ApplicationId = applicationId;
+            this.CreationTime = creationTime;
+            this.Status = status;
+            this.ApplicationType = applicationType;
+            this.ApplicationDescription = applicationDescription;
+            this.CreatedByUser = createdByUser;
+        }
+    }
+
+    public class ApplicationDAL
+    {
+        private static readonly string _connectionString = "Server=localhost;Database=MoveSmart;User Id=root;Password=ahmedroot;";
+
+        private MySqlConnection GetConnection()
+        {
+            return new MySqlConnection(_connectionString);
+        }
+
+        private MySqlCommand GetCommand(string query, MySqlConnection conn)
+        {
+            var command = new MySqlCommand(query, conn);
+            command.CommandType = CommandType.Text;
+            return command;
+        }
+
+        private Application MapApplication(DbDataReader reader)
+        {
+            return new Application
+            (
+                reader.GetInt32("ApplicationID"),
+                reader.GetDateTime("CreationDate"),
+                reader.GetInt32("Status"),
+                reader.GetInt32("ApplicationType"),
+                reader.GetString("ApplicationDescription"),
+                reader.GetInt32("CreatedByUser")
+            );
+        }
+
+        public async Task<List<Application>> GetAllApplicationsAsync()
+        {
+            var applicationsList = new List<Application>();
+
+            await using (var conn = GetConnection())
+            {
+                var query = @"
+                            SELECT ApplicationID, CreationDate, Status, ApplicationType, ApplicationDescription, CreatedByUser
+                            FROM applications";
+
+                using (var cmd = GetCommand(query, conn))
+                {
+                    try
+                    {
+                        await conn.OpenAsync().ConfigureAwait(false);
+
+                        using (var reader = await cmd.ExecuteReaderAsync().ConfigureAwait(false))
+                        {
+                            while (await reader.ReadAsync())
+                            {
+                                applicationsList.Add(MapApplication(reader));
+                            }
+                        }
+                    }
+                    catch (MySqlException ex)
+                    {
+                        throw new Exception("Databse error occurred in GetAllApplicationsAsync.", ex);
+                    }
+                }
+            }
+
+            return applicationsList;
+        }
+        
+        public async Task<Application> GetApplicationByIdAsync(int applicationId)
+        {
+            await using (var conn = GetConnection())
+            {
+                var query = @"
+                            SELECT ApplicationID, CreationDate, Status, ApplicationType, ApplicationDescription, CreatedByUser
+                            FROM applications
+                            WHERE ApplicationID = @applicationId";
+
+                using (var cmd = GetCommand(query, conn))
+                {
+                    cmd.Parameters.AddWithValue("@applicationId", applicationId);
+
+                    try
+                    {
+                        await conn.OpenAsync().ConfigureAwait(false);
+                        using (var reader = await cmd.ExecuteReaderAsync().ConfigureAwait(false))
+                        {
+                            if (await reader.ReadAsync())
+                            {
+                                return MapApplication(reader);
+                            }
+                            else
+                            {
+                                return null;
+                            }
+                        }
+                    }
+                    catch (MySqlException ex)
+                    {
+                        throw new Exception("Database error occurred in GetApplicationByIdAsync.", ex);
+                    }
+                }
+            }
+        }
+
+        public async Task<List<Application>> GetApplicationsByApplicationTypeAsync(int applicationType)
+        {
+            var applicationsList = new List<Application>();
+
+            await using (var conn = GetConnection())
+            {
+                var query = @"
+                            SELECT ApplicationID, CreationDate, Status, ApplicationType, ApplicationDescription, CreatedByUser
+                            FROM applications 
+                            WHERE ApplicationType = @applicationType";
+
+                using (var cmd = GetCommand(query, conn))
+                {
+                    cmd.Parameters.AddWithValue("@applicationType", applicationType);
+
+                    try
+                    {
+                        await conn.OpenAsync().ConfigureAwait(false);
+
+                        using (var reader = await cmd.ExecuteReaderAsync().ConfigureAwait(false))
+                        {
+                            while (await reader.ReadAsync())
+                            {
+                                applicationsList.Add(MapApplication(reader));
+                            }
+                        }
+                    }
+                    catch (MySqlException ex)
+                    {
+                        throw new Exception("Databse error occurred in GetApplicationsByApplicationTypeAsync.", ex);
+                    }
+                }
+            }
+
+            return applicationsList;
+        }
+
+        public async Task<List<Application>> GetApplicationsByCreatedByUserAsync(int createdByUser)
+        {
+            var applicationsList = new List<Application>();
+
+            await using (var conn = GetConnection())
+            {
+                var query = @"
+                            SELECT ApplicationID, CreationDate, Status, ApplicationType, ApplicationDescription, CreatedByUser
+                            FROM applications 
+                            WHERE CreatedByUser = @createdByUser";
+
+                using (var cmd = GetCommand(query, conn))
+                {
+                    cmd.Parameters.AddWithValue("@createdByUser", createdByUser);
+
+                    try
+                    {
+                        await conn.OpenAsync().ConfigureAwait(false);
+
+                        using (var reader = await cmd.ExecuteReaderAsync().ConfigureAwait(false))
+                        {
+                            while (await reader.ReadAsync())
+                            {
+                                applicationsList.Add(MapApplication(reader));
+                            }
+                        }
+                    }
+                    catch (MySqlException ex)
+                    {
+                        throw new Exception("Databse error occurred in GetApplicationsByCreatedByUserAsync.", ex);
+                    }
+                }
+            }
+
+            return applicationsList;
+        }
+
+        public async Task<int> CreateApplicationAsync(Application application)
+        {
+            await using (var conn = GetConnection())
+            {
+                var query = @"
+                    INSERT INTO applications (CreationDate, Status, ApplicationType, ApplicationDescription, CreatedByUser) 
+                    VALUES (@creationDate, @status, @applicationType, @applicationDescription, @createdByUser);
+                    SELECT LAST_INSERT_ID();";
+
+                using (var cmd = GetCommand(query, conn))
+                {
+                    cmd.Parameters.AddWithValue("@creationDate", application.CreationTime);
+                    cmd.Parameters.AddWithValue("@status", application.Status);
+                    cmd.Parameters.AddWithValue("@applicationType", application.ApplicationType);
+                    cmd.Parameters.AddWithValue("@applicationDescription", application.ApplicationDescription);
+                    cmd.Parameters.AddWithValue("@createdByUser", application.CreatedByUser);
+
+                    try
+                    {
+                        await conn.OpenAsync().ConfigureAwait(false);
+                        var newApplicationId = Convert.ToInt32(await cmd.ExecuteScalarAsync().ConfigureAwait(false));
+
+                        return newApplicationId;
+                    }
+                    catch (MySqlException ex)
+                    {
+                        throw new Exception("Databse error occurred in CreateApplicationAsync.", ex);
+                    }
+                }
+            }
+        }
+
+        public async Task<bool> UpdateApplicationAsync(Application application)
+        {
+            await using (var conn = GetConnection())
+            {
+                var query = @"
+                    UPDATE applications
+                    SET 
+                        CreationDate = @creationDate,
+                        Status = @status,
+                        ApplicationType = @applicationType,
+                        ApplicationDescription = @applicationDescription,
+                        CreatedByUser = @createdByUser
+                    WHERE
+                        ApplicationID = @applicationId";
+
+                using (var cmd = GetCommand(query, conn))
+                {
+                    cmd.Parameters.AddWithValue("@creationDate", application.CreationTime);
+                    cmd.Parameters.AddWithValue("@status", application.Status);
+                    cmd.Parameters.AddWithValue("@applicationType", application.ApplicationType);
+                    cmd.Parameters.AddWithValue("@applicationDescription", application.ApplicationDescription);
+                    cmd.Parameters.AddWithValue("@createdByUser", application.CreatedByUser);
+                    cmd.Parameters.AddWithValue("@applicationId", application.ApplicationId);
+
+                    try
+                    {
+                        await conn.OpenAsync().ConfigureAwait(false);
+                        var rowsAffected = await cmd.ExecuteNonQueryAsync().ConfigureAwait(false);
+
+                        return rowsAffected > 0;
+                    }
+                    catch (MySqlException ex)
+                    {
+                        throw new Exception("Database error occurred in UpdateApplicationAsync.", ex);
+                    }
+                }
+            }
+        }
+        
+        public async Task<bool> DeleteApplicationAsync(int applicationId)
+        {
+            await using (var conn = GetConnection())
+            {
+                var query = "DELETE FROM applications WHERE ApplicationID = @applicationId";
+                using (var cmd = GetCommand(query, conn))
+                {
+                    cmd.Parameters.AddWithValue("@applicationId", applicationId);
+
+                    try
+                    {
+                        await conn.OpenAsync().ConfigureAwait(false);
+                        var rowsAffected = await cmd.ExecuteNonQueryAsync().ConfigureAwait(false);
+
+                        return rowsAffected > 0;
+                    }
+                    catch (MySqlException ex)
+                    {
+                        throw new Exception("Database error occurred in DeleteApplicationAsync.", ex);
+                    }
+                }
+            }
+        }
+
+    }
+}
---
+++ Back-End (APIs)/MoveSmart/DataAccessLayer/Repositories/UserDAL.cs
@@ -0,0 +1,258 @@
+﻿using System;
+using System.Data;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+using System.Data.Common;
+
+namespace DataAccessLayer.Repositories
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public string NationalNo { get; set; }
+        public string Name { get; set; }
+        public string Role { get; set; }
+        public int AccessRight { get; set; }
+
+        public User(int userId, string nationalNo, string name, string role, int accessRight)
+        {
+            this.UserId = userId;
+            this.NationalNo = nationalNo;
+            this.Name = name;
+            this.Role = role;
+            this.AccessRight = accessRight;
+        }
+    }
+
+    public class UserDAL
+    {
+        private static readonly string _connectionString = "Server=localhost;Database=MoveSmart;User Id=root;Password=ahmedroot;";
+
+        //public UserDAL(IConfiguration config)
+        //{
+        //    _connectionString = config.GetConnectionString("DefaultConnection");
+        //}
+
+        private MySqlConnection GetConnection()
+        {
+            return new MySqlConnection(_connectionString);
+        }
+
+        private MySqlCommand GetCommand(string query, MySqlConnection conn)
+        {
+            var command = new MySqlCommand(query, conn);
+            command.CommandType = CommandType.Text;
+            return command;
+        }
+        
+        private User MapUser(DbDataReader reader)
+        {
+            return new User
+            (
+                reader.GetInt32("UserID"),
+                reader.GetString("NationalNo"),
+                reader.GetString("Name"),
+                reader.GetString("Role"),
+                reader.GetInt32("AccessRight")
+            );
+        }
+
+        public async Task<List<User>> GetAllUsersAsync()
+        {
+            var usersList = new List<User>();
+
+            await using (var conn = GetConnection())
+            {
+                var query = "SELECT UserID, NationalNo, Name, Role, AccessRight FROM users";
+
+                using (var cmd = GetCommand(query, conn))
+                {
+                    try
+                    {
+                        await conn.OpenAsync().ConfigureAwait(false);
+
+                        using (var reader = await cmd.ExecuteReaderAsync().ConfigureAwait(false))
+                        {
+                            while (await reader.ReadAsync())
+                            {
+                                usersList.Add(MapUser(reader));
+                            }
+                        }
+                    }
+                    catch (MySqlException ex)
+                    {
+                        throw new Exception("Databse error occurred in GetAllUsersAsync.", ex);
+                    }
+                }
+
+            }
+
+            return usersList;
+
+        }
+
+        public async Task<User> GetUserByIdAsync(int userId)
+        {
+            await using (var conn = GetConnection())
+            {
+                var query = "SELECT UserID, NationalNo, Name, Role, AccessRight FROM users WHERE UserID = @userId";
+
+                using (var command = GetCommand(query, conn))
+                {
+                    command.Parameters.Add("@userId", MySqlDbType.Int32).Value = userId;
+
+                    try
+                    {
+                        await conn.OpenAsync().ConfigureAwait(false);
+                        using (var reader = await command.ExecuteReaderAsync().ConfigureAwait(false))
+                        {
+                            if (await reader.ReadAsync())
+                            {
+                                return MapUser(reader);
+                            }
+                            else
+                            {
+                                return null;
+                            }
+                        }
+                    }
+                    catch (MySqlException ex)
+                    {
+                        throw new Exception("Database error occurred in GetUserByIdAsync.", ex);
+                    }
+                }
+            }
+        }
+
+        public async Task<User> GetUserByNationalNoAsync(string nationalNo)
+        {
+            await using (var conn = GetConnection())
+            {
+                var query = "SELECT UserID, NationalNo, Name, Role, AccessRight FROM users WHERE NationalNo = @nationalNo";
+
+                using (var command = GetCommand(query, conn))
+                {
+                    command.Parameters.Add("@nationalNo", MySqlDbType.VarChar).Value = nationalNo;
+
+                    try
+                    {
+                        await conn.OpenAsync().ConfigureAwait(false);
+                        using (var reader = await command.ExecuteReaderAsync().ConfigureAwait(false))
+                        {
+                            if (await reader.ReadAsync())
+                            {
+                                return MapUser(reader);
+                            }
+                            else
+                            {
+                                return null;
+                            }
+                        }
+                    }
+                    catch (MySqlException ex)
+                    {
+                        throw new Exception("Database error occurred in GetUserByNationalNoAsync.", ex);
+                    }
+                }
+            }
+        }
+
+        public async Task<int> CreateUserAsync(User user)
+        {
+            await using (var conn = GetConnection())
+            {
+
+                var query = @"
+                    INSERT INTO users (NationalNo, Name, Role, AccessRight) 
+                    VALUES (@NationalNo, @Name, @Role, @AccessRight);
+                    SELECT LAST_INSERT_ID();";
+
+                using (var command = GetCommand(query, conn))
+                {
+                    command.Parameters.Add("@NationalNo", MySqlDbType.VarChar).Value = user.NationalNo;
+                    command.Parameters.Add("@Name", MySqlDbType.VarChar).Value = user.Name;
+                    command.Parameters.Add("@Role", MySqlDbType.VarChar).Value = user.Role;
+                    command.Parameters.Add("@AccessRight", MySqlDbType.Int64).Value = user.AccessRight;
+
+                    try
+                    {
+
+                        await conn.OpenAsync().ConfigureAwait(false);
+                        var newUserId = Convert.ToInt32(await command.ExecuteScalarAsync().ConfigureAwait(false));
+
+                        return newUserId;
+                    }
+                    catch (MySqlException ex)
+                    {
+                        throw new Exception("Database error occurred in CreateUserAsync.", ex);
+                    }
+                }
+            }
+        }
+
+        public async Task<bool> UpdateUserAsync(User user)
+        {
+            await using (var conn = GetConnection())
+            {
+                var query = @"
+                    UPDATE users 
+                    SET 
+                        NationalNo = @NationalNo, 
+                        Name = @Name, 
+                        Role = @Role, 
+                        AccessRight = @AccessRight 
+                    WHERE 
+                        UserID = @UserID;";
+
+                using (var command = GetCommand(query, conn))
+                {
+                    command.Parameters.Add("@NationalNo", MySqlDbType.VarChar).Value = user.NationalNo;
+                    command.Parameters.Add("@Name", MySqlDbType.VarChar).Value = user.Name;
+                    command.Parameters.Add("@Role", MySqlDbType.VarChar).Value = user.Role;
+                    command.Parameters.Add("@AccessRight", MySqlDbType.Int32).Value = user.AccessRight;
+                    command.Parameters.Add("@UserID", MySqlDbType.Int32).Value = user.UserId;
+
+                    try
+                    {
+
+                        await conn.OpenAsync().ConfigureAwait(false);
+                        var rowsAffected = await command.ExecuteNonQueryAsync().ConfigureAwait(false);
+
+                        return rowsAffected > 0;
+                    }
+                    catch (MySqlException ex)
+                    {
+                        throw new Exception("Database error occurred in UpdateUserAsync.", ex);
+                    }
+                }
+            }
+        }
+
+        public async Task<bool> DeleteUserAsync(int userId)
+        {
+            await using (var conn = GetConnection())
+            {
+                var query = "DELETE FROM users WHERE UserID = @UserId";
+                using (var command = GetCommand(query, conn))
+                {
+                    command.Parameters.Add("@UserId", MySqlDbType.Int32).Value = userId;
+                        
+                    try
+                    {
+                        await conn.OpenAsync().ConfigureAwait(false);
+                        var rowsAffected = await command.ExecuteNonQueryAsync().ConfigureAwait(false);
+
+                        return rowsAffected > 0;
+                    }
+                    catch (MySqlException ex)
+                    {
+                        throw new Exception("Database error occurred in DeleteUserAsync.", ex);
+                    }
+                }
+            }
+        }
+    }
+}
